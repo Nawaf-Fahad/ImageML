4c82960faebba08ca5fcc055c9f51d7389c76e4d
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2023-02-13 14:48:40 +0300

    Initial Commit

---
+++ ImageSports/ImageSportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSportsApp.swift
+//  ImageSports
+//
+//  Created by Nawaf Alotaibi on 13/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ImageSportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
