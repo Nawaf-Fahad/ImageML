9a5b6392294e051caf79725e61385713378d11c9
Nawaf <63883911+Nawaf-Fahad@users.noreply.github.com>
2023-02-14 16:07:37 +0300

    Core

--- ImageSports/ContentView.swift
+++ ImageSports/ContentView.swift
@@ -8,19 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
+    @State private var isImagePickerDisplay = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+            VStack{
+                HStack{
+                    Button {
+                        self.sourceType = .camera
+                        
+                        self.isImagePickerDisplay.toggle()
+
+                       
+                        
+                    }label: {
+                    Image(systemName: "camera.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24,height: 24)
+                    
+                }
+                    Spacer(minLength: 0)
+                    Text("Scary hours")
+                        .font(.caption)
+                    Spacer(minLength: 0)
+
+                    Button {
+                        self.sourceType = .photoLibrary
+                        self.isImagePickerDisplay.toggle()
+                        
+                    } label: {
+                        Image(systemName: "photo.on.rectangle")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24,height: 24)
+                        
+                    }
+                    
+                }
+                .padding()
+                
+                if selectedImage != nil {
+                    Image(uiImage: selectedImage!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 350, height: 450)
+                    
+                } else{
+                    Image("Ghost")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 350, height: 450)
+                }
+                Spacer(minLength: 0)
+            }
+            .sheet(isPresented: $isImagePickerDisplay) {
+            ImagePickerView(selectedImage: $selectedImage, sourceType: self.sourceType)
         }
-        .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
---
+++ ImageSports/Coordinator.swift
@@ -0,0 +1,23 @@
+//
+//  Coordinator.swift
+//  ImageSports
+//
+//  Created by Nawaf Alotaibi on 14/02/2023.
+//
+
+import UIKit
+
+class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    var picker: ImagePickerView
+    
+    init(picker: ImagePickerView) {
+        self.picker = picker
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else { return }
+        self.picker.selectedImage = selectedImage
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+    
+}
